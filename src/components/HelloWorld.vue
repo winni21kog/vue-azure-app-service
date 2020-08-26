64c345078a8ad209056ea277f8762b9d0a296608
Alan <winni21kog@gmail.com>
2020-08-26 15:53:53 +0800

    add dave img

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,10 +2,9 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
     </p>
+    <img alt="Dave" src="../assets/dave.jpg">
     <h3>Installed CLI Plugins</h3>
     <ul>
       <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
